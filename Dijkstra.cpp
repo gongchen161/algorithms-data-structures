c578f3161c2217d7b4fb55da9d13e940cf2625cf
gongchen161 <gong.chen161@gmail.com>
2021-10-06 21:22:57 -0400

    Add a test case for Dijkstra

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -4,17 +4,20 @@
 using namespace std;
 
 
-//adjList: the ith row is vertex i's (neighbor, weight)
-//return: a vector containing the shortest distance from start to vetex i
-vector<int> dijkstra(const vector< vector<pair<int,int>> > adjList, int start) {
-    vector<int> dist(adjList.size() + 1);
+// Find the shortest path from start to all the other nodes
+// adjList[i] = vector of (neighborNodeIdx, weight) of the ith node
+vector<int> dijkstra(const vector< vector< pair<int,int> > >& adjList, int start) {
+    vector<int> dist(adjList.size());
+
     for (size_t i = 0; i<adjList.size(); ++i){
-        dist[i] = numeric_limits<int>::max();
+        dist[i] = INT_MAX;
     }
+    
     dist[start] = 0;
     
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater< pair<int,int> > > pq;
+    priority_queue< pair<int,int>, vector<pair<int,int> >, greater< pair<int,int> > > pq;
     
+    // push a pair of (distance, nodeIdx)
     pq.push(make_pair(0, start));
     
     while(!pq.empty()) {
@@ -24,11 +27,12 @@ vector<int> dijkstra(const vector< vector<pair<int,int>> > adjList, int start) {
         int v = front.second;
         
         if (w <= dist[v]) {
-            for (size_t i = 0; i < adjList[v].size(); i++) {
-                pair<int,int> cur = adjList[v][i];
-                if(dist[v] + cur.second < dist[cur.first]){
-                    dist[cur.first] = dist[v] + cur.second;
-                    pq.push(make_pair(dist[cur.first], cur.first));
+            for (auto& neighbor : adjList[v]) {
+                
+                if(dist[v] + neighbor.second < dist[neighbor.first]){
+                    // found a shorter path
+                    dist[neighbor.first] = dist[v] + neighbor.second;
+                    pq.push(make_pair(dist[neighbor.first], neighbor.first));
                     
                 }
             }
@@ -38,4 +42,43 @@ vector<int> dijkstra(const vector< vector<pair<int,int>> > adjList, int start) {
     }
     
     return dist;
+}
+
+vector< vector< pair<int,int> > > convertAdjacencyMatrixToAdjacencyList(const vector< vector<int> >& matrix) {
+
+    vector< vector< pair<int,int> > > res(matrix.size());
+
+    for (size_t i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[i].size(); j++) {
+            if (matrix[i][j] != 0) {
+                res[i].push_back(make_pair(j, matrix[i][j]));
+            }
+        }
+    }
+    return res;
+}
+
+int main() {
+
+    // this test case was borrowed from
+    // https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
+    vector<int> dist = dijkstra( convertAdjacencyMatrixToAdjacencyList( { 
+        { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+        { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+        { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+        { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+        { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+        { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+        { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+        { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+        { 0, 0, 2, 0, 0, 0, 6, 7, 0 } }
+    ), 0);
+
+    vector<int> ans  = {0, 4, 12, 19, 21, 11, 9, 8, 14};
+    assert(dist.size() == ans.size());
+    for (int i = 0; i < ans.size(); i++) {
+         assert(dist[i] == ans[i]);
+    }
+   
+
 }
